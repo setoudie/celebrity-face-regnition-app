c22ae5a36924a5b412aae408fe5e0631adba3354
setoudie <setoudie@gmail.com>
2024-09-12 11:44:00 +0000

    predict function 0.0.3

--- ml_models/test0.py
+++ ml_models/test0.py
@@ -1,21 +1,22 @@
 
 
 results = {
-           'kate_beckinsale': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'lauren_cohan': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'dwayne_johnson': {'face_1': [False, True, True, False, False, True, False, False, False, False, True, True, False, False, True, True, False, True, False, False]},
-           'madonna': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'sofia_vergara': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'ben_afflek': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'anne_hathaway': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'arnold_schwarzenegger': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'mindy_kaling': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'elton_john': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'jerry_seinfeld': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'will_smith': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'keanu_reeves': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
-           'simon_pegg': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]}
-           }
+  'kate_beckinsale': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'lauren_cohan': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'dwayne_johnson': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'madonna': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'sofia_vergara': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'ben_afflek': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'anne_hathaway': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'arnold_schwarzenegger': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'mindy_kaling': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'elton_john': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'jerry_seinfeld': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'will_smith': {'face_1': [True, False, False, False, True, True, False, True, False, False, False, False, True, False, False, True, False, False, False, False, True]},
+  'keanu_reeves': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]},
+  'simon_pegg': {'face_1': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]}
+}
 
-taux = (results['dwayne_johnson']['face_1'].count(True)/len(results['dwayne_johnson']['face_1']))*100
-print(taux)
+
+taux = (results['will_smith']['face_1'].count(True)/len(results['will_smith']['face_1']))*100
+print(results['will_smith']['face_1'].count(False))

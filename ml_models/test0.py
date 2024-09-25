c7a206db13012c3a1a8237141f94ce89ce50a3b1
setoudie <setoudie@gmail.com>
2024-09-25 13:42:45 +0000

    calcul max_predict_score test.py

--- ml_models/test0.py
+++ ml_models/test0.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 results = {
   'kate_beckinsale': {
@@ -127,6 +127,177 @@ results = {
 }
 
 
+scor_result = {
+  'kate_beckinsale': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'lauren_cohan': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 7.14,
+    'face_4': 0.0
+  },
+  'dwayne_johnson': {
+    'face_2': 0.0,
+    'face_6': 5.0,
+    'face_1': 30.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 40.0,
+    'face_4': 0.0
+  },
+  'madonna': {
 
-taux = (results['will_smith']['face_1'].count(True)/len(results['will_smith']['face_1']))*100
-print(results['will_smith']['face_1'].count(True))
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'sofia_vergara': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 4.35,
+    'face_4': 0.0
+  },
+  'ben_afflek': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'anne_hathaway': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'arnold_schwarzenegger': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 8.7,
+    'face_4': 0.0
+  },
+  'mindy_kaling': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'elton_john': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'jerry_seinfeld': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'will_smith': {
+    'face_2': 0.0,
+    'face_6': 19.05,
+    'face_1': 19.05,
+    'face_3': 14.29,
+    'face_7': 9.52,
+    'face_5': 0.0,
+    'face_4': 4.76
+  },
+  'keanu_reeves': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  },
+  'simon_pegg': {
+    'face_2': 0.0,
+    'face_6': 0.0,
+    'face_1': 0.0,
+    'face_3': 0.0,
+    'face_7': 0.0,
+    'face_5': 0.0,
+    'face_4': 0.0
+  }
+}
+
+
+# fonction pour associer un nom à la face detecter
+def find_celebrity(my_results_dico):
+  pass
+
+
+# ----------------------------
+def calculate_score(my_face_bool_list):
+  # pass
+  true_val = my_face_bool_list.count(True)
+  # false_val = my_face_bool_list.count(False)
+  total_val = len(my_face_bool_list)
+  score = 100*true_val/total_val
+  return round(score, 2)
+
+#-------------------------------
+def get_celeb_faces_score(my_celeb_result_dict):
+  celeb_faces_scores = dict()
+  for face in my_celeb_result_dict:
+    celeb_faces_scores[face] = calculate_score(my_celeb_result_dict[face])
+  # print(celeb_faces_scores)
+  return celeb_faces_scores
+
+def get_all_celebs_faces_scores(rslt):
+  all_celebs_scores = dict()
+  for celeb in rslt:
+    all_celebs_scores[celeb] = get_celeb_faces_score(rslt[celeb])
+  # print(all_celebs_scores)
+
+  return all_celebs_scores
+
+def calculate_mean_score(raw_data):
+  faces_scores = get_all_celebs_faces_scores(raw_data)
+  mean_score_dict = dict()
+  
+  for celeb in raw_data:
+    celeb_mean_score = round(sum([*faces_scores[celeb].values()]) / len(faces_scores[celeb]), 2)
+    mean_score_dict[celeb] = celeb_mean_score
+
+  return pd.Series(mean_score_dict)
+
+
+df = calculate_mean_score(results)
+print(df['will_smith'])
